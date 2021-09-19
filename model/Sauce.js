4ba59078db5a020b92bdd862a8393a540d0deccb
Mahfaud <mahfaud.cherdoud@gmail.com>
2021-09-19 23:05:01 +0200

    Ajout d'un model pour les sauces et ajout d'un middleware pour les fichiers statics

---
+++ model/Sauce.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const requiredString = {
+    type: String,
+    required: true
+}
+
+const requiredNumber = {
+    type: Number,
+    required: true
+}
+
+const sauceSchema = new mongoose.Schema({
+        userId: requiredString,
+        name: requiredString,
+        manufacturer: requiredString,
+        description: requiredString,
+        mainPepper: requiredString,
+        imageUrl: requiredString,
+        heat: requiredNumber,
+        likes: requiredNumber,
+        dislikes: requiredNumber,
+        // Array de string d'users Id
+        usersLiked: [requiredString],
+        usersDisliked: [requiredString]
+    
+})
+
+module.exports = mongoose.model("Sauce", sauceSchema)
